f11c92aabd95397b3c61cc41d6510eb6b73f39be
Alberto Vona <alberto.vona24@gmail.com>
2019-10-10 20:52:45 +0200

    Bug fixes and minor improvements
    Added support to stream.py for multi-threading

--- stream.py
+++ stream.py
@@ -1,30 +1,45 @@
 """Capture video and stream it to a webpage"""
-#pylint: disable=no-name-in-module
+# pylint: disable=no-name-in-module, unused-variable
 from base64 import b64encode
-from socket import AF_INET, SOCK_STREAM, socket, gethostbyname, gethostname
+from socket import AF_INET, SOCK_STREAM, gethostbyname, gethostname, socket
+from threading import Thread
 
-from cv2 import (VideoCapture, destroyAllWindows, imencode, imshow, waitKey)
+from cv2 import VideoCapture, imencode
 
 SOCK = socket(AF_INET, SOCK_STREAM)
-SOCK.bind((gethostbyname(gethostname()), 7503))
+HOST = gethostbyname(gethostname())
+PORT = int(input("Port: "))
+SOCK.bind((HOST, PORT))
+print(f"{HOST}:{PORT}")
 SOCK.listen(1)
 
 STREAM = VideoCapture(0)
 
 
+def server():
+    """Stream live video"""
+    ret, frame = STREAM.read()
+    retval, buffer = imencode('.jpg', frame)
+    server_thread = Thread(target=server, daemon=True)
+    conn, addr = SOCK.accept()
+    server_thread.start()
+    conn.settimeout(2)
+    try:
+        data = conn.recv(512).decode().split(' ')
+        if data[0] == "GET":
+            with open("stream.html", "rb") as website:
+                conn.sendall(b"HTTP/1.1 200 OK\n\n" + website.read())
+        elif data[0] == "VIDEO":
+            conn.sendall(b"HTTP/1.1 200 OK\n\n" + b64encode(buffer))
+    except ConnectionResetError:
+        return
+    conn.close()
+
+
+server()
 while True:
-    RET, FRAME = STREAM.read()
-    RETVAL, BUFFER = imencode('.jpg', FRAME)
-    CONN, ADDR = SOCK.accept()
-    DATA = CONN.recv(512).decode().split(' ')
-    if DATA[0] == "GET":
-        with open("test.html", "rb") as website:
-            CONN.sendall(b"HTTP/1.1 200 OK\n\n" + website.read())
-    elif DATA[0] == "VIDEO":
-        CONN.sendall(b"HTTP/1.1 200 OK\n\n" + b64encode(BUFFER))
-    CONN.close()
-    imshow('frame', FRAME)
-    if waitKey(1) & 0xFF == ord('q'):
+    try:
+        pass
+    except KeyboardInterrupt:
         break
 STREAM.release()
-destroyAllWindows()
